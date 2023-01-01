3532ea185999c635f9819813eb65dc35f3f2d5c3
Chaskar Shailesh <chaskarshailesh1981@gmail.com>
2023-01-01 13:15:06 +0530

    updated files:

--- terraform/aks cluster/providers.tf
+++ terraform/aks cluster/providers.tf
@@ -16,7 +16,5 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "556a8191-80bf-4752-b4de-c25ad263abf2"
-  client_id       = "d6adfb30-53e3-4719-aba8-7ebff0416131"
-  client_secret   = "dPM8Q~GUdkvISvfUvsWQ9BsJemSGgRe6r.CSFbin"
   tenant_id       = "a9313beb-02e5-4337-ad1e-e101ac3ec7ca"
 }
--- terraform/aks cluster/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aks_service_principal_app_id = "d6adfb30-53e3-4719-aba8-7ebff0416131"
-aks_service_principal_client_secret = "dPM8Q~GUdkvISvfUvsWQ9BsJemSGgRe6r.CSFbin"
