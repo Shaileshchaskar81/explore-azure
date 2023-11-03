8c631ec5adebc91c7867983660161f15fa39c5b4
Shailesh Chaskar <chaskarshailesh1981@gmail.com>
2023-11-03 12:55:27 +0530

    upper case

--- firstapp/firstapp.py
+++ firstapp/firstapp.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('first-app')
 @app.route('/')
 def hello():
-  return "Hello World! Lets sail together - First App.\n"
+  return "Hello World! Lets sail together - first App.\n"
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
--- firstapp/test_firstapp.py
+++ firstapp/test_firstapp.py
@@ -2,6 +2,6 @@ import unittest
 from firstapp import hello
 class TestFirstApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), "Hello World! Lets sail together - First App.\n")
+        self.assertEqual(hello(), "Hello World! Lets sail together - first App.\n")
 if __name__ == '__main__':
   unittest.main()
